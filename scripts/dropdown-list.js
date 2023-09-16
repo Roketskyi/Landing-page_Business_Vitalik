a1a7d44441449fc77019c6684e3c797f8039b8c7
Roman <romanroketskiy05@gmail.com>
2023-09-16 16:06:32 +0300

    update

--- scripts/dropdown-list.js
+++ scripts/dropdown-list.js
@@ -1,8 +1,8 @@
-const dropdownButtons = document.querySelectorAll('.showDropdown');
+// const dropdownButtons = document.querySelectorAll('.showDropdown');
 
-dropdownButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const dropdown = button.nextElementSibling;
-        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-    });
-});
+// dropdownButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const dropdown = button.nextElementSibling;
+//         dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+//     });
+// });
