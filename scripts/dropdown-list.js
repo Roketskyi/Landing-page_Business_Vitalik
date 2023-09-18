65058852f1d60d7cd8b9097012d7c1ed19a7e314
Roman <romanroketskiy05@gmail.com>
2023-09-18 12:36:47 +0300

    update

--- scripts/dropdown-list.js
+++ scripts/dropdown-list.js
@@ -1,16 +1,28 @@
 const brickButtons = document.querySelectorAll(".brickButton");
-    const brickLists = document.querySelectorAll(".brickList");
+const brickLists = document.querySelectorAll(".brickList");
 
-    // Додайте обробник подій для кожної кнопки
-    brickButtons.forEach(function (button, index) {
-        button.addEventListener("click", function () {
-            // Перевірте, чи список схований або видимий
-            if (brickLists[index].classList.contains("hidden")) {
-                // Відобразіть список
-                brickLists[index].classList.remove("hidden");
-            } else {
-                // Сховайте список
-                brickLists[index].classList.add("hidden");
-            }
-        });
+brickButtons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
+        if (brickLists[index].classList.contains("hidden")) {
+            brickLists[index].classList.remove("hidden");
+            button.classList.add("active"); // Додайте клас "active" для активних кнопок
+        } else {
+            brickLists[index].classList.add("hidden");
+            button.classList.remove("active"); // Видаліть клас "active" для неактивних кнопок
+        }
     });
+
+    button.addEventListener("mouseenter", function () {
+        if (!button.classList.contains("active")) {
+            button.style.backgroundColor = "#67bb44"; // Зелений колір при наведенні на неактивну кнопку
+            button.style.color = "#fff";
+        }
+    });
+
+    button.addEventListener("mouseleave", function () {
+        if (!button.classList.contains("active")) {
+            button.style.backgroundColor = "#f5f5f5"; // Поверніть початковий колір при виході з кнопки
+            button.style.color = "#333";
+        }
+    });
+});
