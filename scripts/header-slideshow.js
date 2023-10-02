45087f951c6415a2d968f42c8ecb28dc04562829
Roman <romanroketskiy05@gmail.com>
2023-10-02 11:01:27 +0300

    update

--- scripts/header-slideshow.js
+++ scripts/header-slideshow.js
@@ -1,11 +1,18 @@
-const images = ["/images/DSC_2070.JPG", "/images/IMG_1865.JPG", "/images/IMG_1865.JPG"];
+const images = [
+    "/images/DSC_2070.JPG",
+    "/images/IMG_1865.JPG",
+    "/images/DSCF2347.JPG",
+    "/images/new1.jpg",
+    "/images/new2.jpg",
+    "/images/new3.jpg"
+];
 
 let slideIndex = 0;
 
 function changeImage() {
-slideIndex = (slideIndex + 1) % images.length;
-const headerImage = document.getElementById("headerImage");
-headerImage.src = images[slideIndex];
+    slideIndex = (slideIndex + 1) % images.length;
+    const headerImage = document.getElementById("headerImage");
+    headerImage.src = images[slideIndex];
 }
 
-setInterval(changeImage, 3000); // Змінювати фотографію кожні 3 секунди
+setInterval(changeImage, 4000);
