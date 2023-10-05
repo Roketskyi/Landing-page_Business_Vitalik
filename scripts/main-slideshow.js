e41d0dade9192368e8e957cddb57ca52bede476d
Roman <romanroketskiy05@gmail.com>
2023-10-06 00:34:33 +0300

    update

---
+++ scripts/main-slideshow.js
@@ -0,0 +1,31 @@
+const mainImages = [
+    "/images/main.jpg",
+    "/images/Byklet_00001.png",
+    "/images/IMG_3516.JPG",
+    "/images/IMG_3160.JPG",
+    "/images/IMG_3150.JPG",
+    "/images/hotomir.JPG"
+];
+
+let mainImageSlide = 0;
+
+function changeImage() {
+    mainImageSlide = (mainImageSlide + 1) % mainImages.length;
+    const headerImage = document.getElementById("mainImageSlide");
+
+    // Оновлюємо посилання FancyBox для всіх фотографій
+    const fancyboxLinks = document.querySelectorAll('[data-fancybox="gallery"]');
+    fancyboxLinks.forEach((link, index) => {
+        link.href = mainImages[(mainImageSlide + index - 1) % mainImages.length];
+    });
+
+    headerImage.style.opacity = 0;
+
+    setTimeout(() => {
+        headerImage.src = mainImages[mainImageSlide];
+        
+        headerImage.style.opacity = 1;
+    }, 1500);
+}
+
+setInterval(changeImage, 8000);
