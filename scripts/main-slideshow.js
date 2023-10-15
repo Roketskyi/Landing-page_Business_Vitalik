ebbce79f3c0710bb1a887d7b3c0672f5585e8054
Roman <romanroketskiy05@gmail.com>
2023-10-15 11:59:22 +0300

    update

--- scripts/main-slideshow.js
+++ scripts/main-slideshow.js
@@ -3,7 +3,13 @@ const mainImages = [
     "./images/IMG_3516.JPG",
     "./images/IMG_3160.JPG",
     "./images/IMG_3150.JPG",
-    "./images/hotomir.JPG"
+    "./images/hotomir.JPG",
+    "./images/DSC_2070.JPG",
+    "./images/IMG_1865.JPG",
+    "./images/DSCF2347.JPG",
+    "./images/new1.jpg",
+    "./images/new2.jpg",
+    "./images/new3.jpg"
 ];
 
 let mainImageSlide = 0;
@@ -11,17 +17,10 @@ let mainImageSlide = 0;
 function changeImage() {
     mainImageSlide = (mainImageSlide + 1) % mainImages.length;
     const headerImage = document.getElementById("mainImageSlide");
-
-    const fancyboxLinks = document.querySelectorAll('[data-fancybox="gallery"]');
-    fancyboxLinks.forEach((link, index) => {
-        link.href = mainImages[(mainImageSlide + index) % mainImages.length];
-    });
-
     headerImage.style.opacity = 0;
 
     setTimeout(() => {
         headerImage.src = mainImages[mainImageSlide];
-        
         headerImage.style.opacity = 1;
     }, 1500);
 }
--- scripts/slider.js
+++ scripts/slider.js
@@ -5,6 +5,7 @@ const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 
 let currentIndex = 0;
+let timer; // Змінна для автоматичної зміни слайдів
 
 // Функція для перемикання слайдів вперед
 function nextSlide() {
@@ -35,9 +36,28 @@ function updateSlider() {
     });
 }
 
+// Функція для початку автоматичної зміни слайдів
+function startAutoSlide() {
+    timer = setInterval(nextSlide, 8000);
+}
+
+// Функція для зупинки автоматичної зміни слайдів
+function stopAutoSlide() {
+    clearInterval(timer);
+}
+
 // Додавання обробників подій на кнопки
-nextButton.addEventListener('click', nextSlide);
-prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', () => {
+    nextSlide();
+    stopAutoSlide(); // Зупиняємо автоматичну зміну слайдів при кліці на кнопку
+    startAutoSlide(); // Починаємо автоматичну зміну слайдів знову
+});
+prevButton.addEventListener('click', () => {
+    prevSlide();
+    stopAutoSlide(); // Зупиняємо автоматичну зміну слайдів при кліці на кнопку
+    startAutoSlide(); // Починаємо автоматичну зміну слайдів знову
+});
 
 // Початкова ініціалізація слайдера
 updateSlider();
+startAutoSlide(); // Запускаємо автоматичну зміну слайдів
