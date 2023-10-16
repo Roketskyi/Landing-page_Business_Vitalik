0870ec76636bf544812f7670c35a80585d43c3d1
Roman <romanroketskiy05@gmail.com>
2023-10-16 12:45:27 +0300

    update

--- scripts/main-slideshow.js
+++ scripts/main-slideshow.js
@@ -1,30 +1,30 @@
 const mainImages = [
-    "./images/Byklet_00001.png",
-    "./images/IMG_3516.JPG",
-    "./images/IMG_3160.JPG",
-    "./images/IMG_3150.JPG",
-    "./images/hotomir.JPG",
-    "./images/DSC_2070.JPG",
-    "./images/IMG_1865.JPG",
-    "./images/DSCF2347.JPG",
-    "./images/new1.jpg",
-    "./images/new2.jpg",
-    "./images/new3.jpg"
+  "./images/Byklet_00001.png",
+  "./images/IMG_3516.JPG",
+  "./images/IMG_3160.JPG",
+  "./images/IMG_3150.JPG",
+  "./images/hotomir.JPG",
+  "./images/DSC_2070.JPG",
+  "./images/IMG_1865.JPG",
+  "./images/DSCF2347.JPG",
+  "./images/new1.jpg",
+  "./images/new2.jpg",
+  "./images/new3.jpg"
 ];
 
 let mainImageSlide = 0;
 
 function changeImage() {
-    mainImageSlide = (mainImageSlide + 1) % mainImages.length;
-    const headerImage = document.getElementById("mainImageSlide");
+  mainImageSlide = (mainImageSlide) % mainImages.length;
+  const headerImage = document.getElementById("mainImageSlide");
 
-    // Змінити прозорість зображення перед зміною src
-    headerImage.style.opacity = 0;
+  // Змінити прозорість зображення перед зміною src
+  headerImage.style.opacity = 0;
 
-    setTimeout(() => {
-      headerImage.src = mainImages[mainImageSlide];
-      headerImage.style.opacity = 1;
-    }, 1500);
+  setTimeout(() => {
+    headerImage.src = mainImages[mainImageSlide];
+    headerImage.style.opacity = 1;
+  }, 1500);
 }
 
 setInterval(changeImage, 8000);
